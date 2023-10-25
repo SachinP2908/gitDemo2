3bc9b67cc0a7601d985abe47e6ad4737e820e3cd
sachinp2910 <sachinpawar2542@gmail.com>
2023-10-25 14:30:30 +0530

    this is git throuth cmd

---
+++ src/abc/Test.java
@@ -0,0 +1,7 @@
+package abc;
+
+public class Test {
+public void m1() 
+{
+	System.out.println("method 1");}
+}
---
+++ src/abc/Test3.java
@@ -0,0 +1,7 @@
+package abc;
+
+public class Test3 {
+public void m1() 
+{
+	System.out.println("this is method 3");}
+}
